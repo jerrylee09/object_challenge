4a04a31ae450ecebd6448ffd7a0459a53ce33053
Jerry Lee <jerry.lee09@yahoo.com>
2016-07-28 10:08:55 -0500

    add first commit

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-var locations = []; // this array will hold your objects
+// this array will hold your objects
+var locations = [
+
+]; 
 
 // object constructor
+var State = function (state, revenue, locationCount) {
+	this.state = state;
+	this.revenue = revenue;
+	this.locationCount = locationCount;
+		this.averageRevenue = function() {
+			var average = this.revenue / this.locationCount;
+			return average;
+		}
+
+}
+
 
 // object instances
-
-// push object instances to locations array
+var minnesota = new State("MN", 300, 3);
+var illinois = new State("IL", 5000, 12);
+var nevada = new State("NV", 25000, 1);
+// // push object instances to locations array
+// locations.push( ["california", 2000, 4 ] );
+// locations.push( ["florida", 1000, 5] );
+locations.push(minnesota);
+locations.push(illinois);
+locations.push(nevada);
 
 // DO NOT MODIFY THIS CODE
+
 $(document).ready(function () {
   locations.forEach(function (element, index) {
     var $li = $('<li></li>');
@@ -17,3 +39,5 @@ $(document).ready(function () {
     $('ol').append($li);
   });
 });
+
+console.log(locations);
